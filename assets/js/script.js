dc8bc0b29befb0885cd27f7ff9eda764273d109f
Maelikah <eva.longoria@gmail.com>
2023-04-17 13:33:06 -0600

    Added modal warning while input validation in input city field

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,29 @@ var apiKey = '4a31ba71a2f681e1cefe6e395d3fc948';
 // Add event listener for the search input when the user presses the Enter key
 document.getElementById("cityInput").addEventListener ("keypress", function(event) {
     // Check for "Enter" key press
-    if (event.keycode === 13) {
-        // Check if input field is empty
+    if (event.key === "Enter") {
         if (this.value.trim() === "") {
+            var warningModal = document.getElementById("warningModal");
+            warningModal.className = "modal is-active";
+            console.log("You need to input data");
+            warningModal.querySelector(".modal-background").addEventListener("click", function() {
+                warningModal.className = "modal";
+            });
+            warningModal.querySelector(".modal-close").addEventListener("click", function () {
+                warningModal.className = "modal";
+                event.stopPropagation();
+            });
+
+        } else {
+            console.log("Enter was pressed");
+            event.preventDefault();
+            cityName = document.getElementById("cityInput").value;
+            console.log("City name is: " +cityName);
 
         }
+        
     }
-    event.preventDefault();
-    cityName = document.getElementById("cityInput").value;
-    console.log("City name is: " +cityName);
+    
 
 });
 
